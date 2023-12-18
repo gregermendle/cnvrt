cb1bedb11af8a3400fbd915fd0fbadeeb6a05463
Greg Jordan <greg@studable.com>
2023-12-18 13:25:48 -0500

    fix: improve gif quality

--- lib/video.ts
+++ lib/video.ts
@@ -2,7 +2,17 @@ import { fetchFile, FFmpeg } from "@ffmpeg/ffmpeg";
 
 export const videoToGif = async (ffmpeg: FFmpeg, file: File) => {
   ffmpeg.FS("writeFile", file.name, await fetchFile(file));
-  await ffmpeg.run("-i", file.name, "-loop", "0", "output.gif");
+  await ffmpeg.run(
+    "-i",
+    file.name,
+    "-loop",
+    "0",
+    "-vf",
+    `fps=30,scale=320:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse`,
+    "output.gif"
+  );
   const data = ffmpeg.FS("readFile", "output.gif");
   return URL.createObjectURL(new Blob([data.buffer], { type: "image/gif" }));
 };
+
+//./ffmpeg.exe -framerate 24 -i C:/tmp/%04d.png -filter_complex "[0:v] scale=w=720:h=-1,split [a][b];[a] palettegen=stats_mode=single [p];[b][p] paletteuse=new=1" output.gif
