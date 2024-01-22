16e99d2d3afe1530abc1254624ff6e37d7e734fb
Greg <greg@studable.com>
2024-01-22 02:53:33 -0500

    fix: preload ffmpeg

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,8 @@ export default function Document() {
     <Html className="h-full" data-theme="light">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preload" href="https://unpkg.com/@ffmpeg/core@0.12.5/dist/umd/ffmpeg-core.js" as="script" />
+        <link rel="preload" href="https://unpkg.com/@ffmpeg/core@0.12.5/dist/umd/ffmpeg-core.wasm" as="script" />
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
