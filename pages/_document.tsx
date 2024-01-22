fa159b8f2be10e8cabf175ae56348b404c952855
Greg <greg@studable.com>
2024-01-22 02:58:04 -0500

    fix: actually preload as fetch

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,8 +5,8 @@ export default function Document() {
     <Html className="h-full" data-theme="light">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preload" href="https://unpkg.com/@ffmpeg/core@0.12.5/dist/umd/ffmpeg-core.js" as="script" />
-        <link rel="preload" href="https://unpkg.com/@ffmpeg/core@0.12.5/dist/umd/ffmpeg-core.wasm" as="script" />
+        <link rel="preload" href="https://unpkg.com/@ffmpeg/core@0.12.5/dist/umd/ffmpeg-core.js" as="fetch" crossOrigin="anonymous" />
+        <link rel="preload" href="https://unpkg.com/@ffmpeg/core@0.12.5/dist/umd/ffmpeg-core.wasm" as="fetch" crossOrigin="anonymous" />
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
